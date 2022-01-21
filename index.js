42abf52144adc56ff9ef506d6a13f4e5a9976546
bogdanvasile7 <84321435+bogdanvasile7@users.noreply.github.com>
2022-01-21 09:31:55 +0200

    added companies endpoint

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
+const companies = [
+    {
+        id: 1, 
+        name: 'SC PROIKAV SRL'
+    },
+    {
+        id: 2, 
+        name: 'SC CLOSURE TECHNOLOGIES SRL'
+    },
+]
+    
+
+app.get('/companies', (req, res) => {
+    res.send(companies);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
